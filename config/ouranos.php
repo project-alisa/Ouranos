ed42f0f0cab04ca31c578a6cdbc0470c04493048
K Miyano <miyano@miyacorata.net>
2019-07-11 01:25:38 +0900

    Renew top page
    かっけえだろ

--- config/ouranos.php
+++ config/ouranos.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'version' => '0.1.0 Azteca'
+    'version' => '0.1.0 THE@TER',
+    'phrase' => 'The handmade Budokan',
+    'cardname' => env('CARD_NAME','shihokitazawa_therapistonboat')
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,104 +1,126 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Ouranos</title>
+    <title>Ouranos</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #eeeeee;
-                color: #afa690;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <style>
+        html, body {
+            background: #544e3e;
+            position: relative;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        #systemname{
+            position: absolute;
+            font-size: 60px;
+            color: #afa690;
+            z-index: 0;
+            top: 45vh;
+            width: 100vw;
+            text-align: center;
+        }
+        #systemname > p{
+            margin: 13px;
+            font-size: 16px;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        main{
+            position: absolute;
+            z-index: 2;
+            bottom: 0;
+            width: 100vw;
+            padding: 20px 0;
+            background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.7));
+            text-align: center;
+            animation: fadein 4.5s ease-in-out;
+        }
+        h1{
+            margin: 10px;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        #admin{
+            position: absolute;
+            bottom: 10px;
+            right: 20px;
+        }
+        #admin > a{
+            color: white;
+            text-decoration: none;
+            font-size: 16px;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        #links > a{
+            color: white;
+            text-shadow: 0 0 4px black;
+            text-decoration: none;
+            font-size: 20px;
+            margin: 0 10px;
+        }
 
-            .content {
-                text-align: center;
-            }
+        #background{
+            position: fixed;
+            overflow: hidden;
+            width: 100vw;
+            height: 100vh;
+            object-fit: cover;
+            object-position: top center;
+            z-index: 1;
+            animation: fadein 4s ease-in-out;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        @keyframes fadein {
+            0%   { opacity: 0; }
+            40%  { opacity: 0; }
+            100% { opacity: 1; }
+        }
+    </style>
+</head>
+<body>
+<img src="{{ asset('image/card/'.config('ouranos.cardname').'.jpg') }}" alt="Background" id="background">
+<div id="systemname">
+    {{ config('app.name') }}
+    <p>
+        Ver {{ config('ouranos.version') }}<br>
+        {{ config('ouranos.phrase') }}
+    </p>
+</div>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 15px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-            }
+<main>
+    <h1><img src="{{ asset('image/mlp_ouranos.png') }}" alt="mlp">
+    </h1>
+    <div id="links">
+        <a href="javascript:void(0)">Idol</a>
+        <a href="javascript:void(0)">List</a>
+        <a href="javascript:void(0)">Unit</a>
+        <a href="javascript:void(0)">Search</a>
+        <a href="https://github.com/project-alisa/Ouranos" target="_blank">GitHub</a>
+    </div>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+    @if (Route::has('login'))
+        <div id="admin">
+            @auth
+                <a href="{{ url('/home') }}">Office</a>
+            @else
+                <a href="{{ route('login') }}">Admin</a>
 
-            #background{
-                position: fixed;
-                overflow: hidden;
-                width: 100vw;
-                height: 100vh;
-                object-fit: cover;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Office</a>
-                    @else
-                        <a href="{{ route('login') }}">Admin</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Ouranos
-                </div>
-
-                <div class="links">
-                    <a href="javascript:void(0)">Idol</a>
-                    <a href="javascript:void(0)">List</a>
-                    <a href="javascript:void(0)">Unit</a>
-                    <a href="javascript:void(0)">Search</a>
-                    <a href="https://github.com/project-alisa/Ouranos" target="_blank">GitHub</a>
-                </div>
-            </div>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
         </div>
-    </body>
+    @endif
+</main>
+
+
+</body>
 </html>
