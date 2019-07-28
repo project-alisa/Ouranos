72543729f4dcc944da3ffb4e49313046d4752184
K Miyano <miyano@miyacorata.net>
2019-07-28 18:32:41 +0900

    Update Idollist

---
+++ resources/views/idol/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app',['title' => __('Idols list'), 'sub' => 'ｼｬﾊﾞﾀﾞﾊﾞｯ (⋈•̀ᴗ•́⋈三(⋈•̀ᴗ•́⋈)三⋈ •̀ᴗ•́⋈)ｼｬﾊﾞﾀﾞﾊﾞｯ'])
+
+@section('content')
+    <div id="contentright">
+        <div class="msgbox">
+            <div class="msgboxtop">Navigation</div>
+            <div class="msgboxbody">
+                <p>
+                    Hoge
+                </p>
+            </div>
+            <div class="msgboxfoot"></div>
+        </div>
+    </div>
+    <div id="contentleft">
+        <div class="msgbox">
+            <div class="msgboxtop">{{ __('Idols list') }}</div>
+            <div class="msgboxbody">
+                @foreach ($idols as $idol)
+                    @php
+                        /** @var \App\Idol $idol */
+                        $icon = asset('image/icon/'.$idol->name_r.'/0.png');
+                    @endphp
+                    <a href="{{ url('/idol/'.$idol->name_r) }}" class="idol">
+                        <img src="{{ $icon }}" class="idolicon" alt="icon" style="border-color: {{ getTypeColor($idol->type) }}">
+                        <div class="idolinfo">
+                            <p class="name">{{ separateString($idol->name,$idol->name_separate) }}</p>
+                            <table>
+                                <tr>
+                                    <th>{{ __('Type') }}</th><td style="width: 80px;font-weight: bold;color: {{ getTypeColor($idol->type) }}">{{ $idol->type }}</td>
+                                    <th>{{ __('Age') }}</th><td style="width: 70px;">{{ $idol->age }}</td>
+                                    <th>{{ __('Birthdate') }}</th><td>{{ convertDateString($idol->birthdate,'ja') }}</td>
+                                    <th>{{ __('Color') }}</th><td style="color: {{ '#'.$idol->color }};width: 100px">■ {{ '#'.$idol->color }}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
+            <div class="msgboxfoot"></div>
+        </div>
+    </div>
+@endsection
