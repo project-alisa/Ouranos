6f5b24002c4291e73b12da952a99b6efc820498e
K Miyano <miyano@miyacorata.net>
2019-08-17 16:08:51 +0900

    Disable user auth

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false , 'reset' => false , 'verify' => false]);
+//Auth::routes(['register' => false , 'reset' => false , 'verify' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('idol','IdolController',['only' => ['index','show']]);
