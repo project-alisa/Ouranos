848d2c9f87d81fb127a51a52af5909024e7464ac
K Miyano <miyano@miyacorata.net>
2019-10-03 01:11:34 +0900

    Add MediaSearch to idol.show

--- resources/views/idol/show.blade.php
+++ resources/views/idol/show.blade.php
@@ -29,7 +29,8 @@
                     <?php /** @var App\Idol $idol */
                     $namestr = separateString($idol->name,$idol->name_separate);
                     if(!empty($idol->subname)) $namestr .= ' ('.$idol->subname.')';
-                    $dateflag = App::isLocale('ja') ? 'ja' : 'slash'; ?>
+                    $dateflag = App::isLocale('ja') ? 'ja' : 'slash';
+                    $urlname = urlencode($idol->name) ?>
                     <th>{{ __('Name') }}</th><td colspan="3">{{ $namestr }}</td>
                     <th>{{ __('CV') }}</th><td>{{ $idol->cv }}</td>
                 </tr>
@@ -100,13 +101,71 @@
             </div>
         </div>
     </div>
-    <div class="msgbox">
-        <div class="msgboxtop">Idol</div>
-        <div class="msgboxbody" style="overflow-x: scroll">
-        <pre>
-            <?php var_dump($idol); ?>
-        </pre>
+    <div id="twinbox">
+        <div class="msgbox" id="contentwide">
+            <div class="msgboxtop">メディア検索</div>
+            <div class="msgboxbody" style="overflow-x: scroll">
+                <h2>Twitterを検索</h2>
+                <div class="buttonbox">
+                    <a href="https://twitter.com/search?q={{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f50e; 通常検索<br><span class="subline">Twitter検索</span>
+                    </a>
+                    <a href="https://twitter.com/search?f=live&q={{ $urlname }}" class="button jwil" target="_blank">
+                        &#x231a; リアルタイム検索<br><span class="subline">すべてのツイートを時系列順に表示</span>
+                    </a>
+                    <a href="https://twitter.com/search?f=user&q={{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f1f5; ユーザー検索<br><span class="subline">プロフィールからユーザーを検索</span>
+                    </a>
+                    <a href="https://twitter.com/search?f=image&q={{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f4f7; 画像検索<br><span class="subline">画像を含むツイートを検索</span>
+                    </a>
+                    <a href="https://twitter.com/search?f=video&q={{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f4f9; 動画検索<br><span class="subline">動画を含むツイートを検索</span>
+                    </a>
+                    <a href="https://twitter.com/search?f=tweet&q={{ $urlname }}%20⚡️️" class="button jwil" target="_blank">
+                        &#x26a1; モーメント検索<br><span class="subline">モーメントを検索</span>
+                    </a>
+                </div>
+                <p class="notification">
+                    Twitter検索は愛称・ニックネームにより表示されるべき結果を表示できない可能性があります。
+                </p>
+                <h2>niconicoを検索</h2>
+                <div class="buttonbox">
+                    <a href="http://www.nicovideo.jp/search/{{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f4fa; 動画をキーワード検索
+                    </a>
+                    <a href="http://www.nicovideo.jp/tag/{{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f4fa; 動画をタグ検索
+                    </a>
+                    <a href="http://seiga.nicovideo.jp/tag/{{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f3a8; 静画をタグ検索
+                    </a>
+                </div>
+                <h2>Pixivを検索</h2>
+                <div class="buttonbox">
+                    <a href="https://www.pixiv.net/search.php?s_mode=s_tag_full&word={{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f3a8; タグで検索<br><span class="subline">Pixiv総合検索</span>
+                    </a>
+                    <a href="https://www.pixiv.net/search.php?s_mode=s_tc&word={{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f3a8; キーワードで検索<br><span class="subline">Pixiv総合検索</span>
+                    </a>
+                    <a href="https://www.pixiv.net/novel/tags.php?tag={{ $urlname }}" class="button jwil" target="_blank">
+                        &#x1f4dd; 書き物をタグで検索<br><span class="subline">Pixiv小説検索</span>
+                    </a>
+                </div>
+            </div>
+            <div class="msgboxfoot"></div>
         </div>
-        <div class="msgboxfoot"></div>
+        <div class="msgbox" id="contentnarrow">
+            <div class="msgboxtop">Inform@tion</div>
+            <div class="msgboxbody">
+                <h2>Hoge</h2>
+                <p>
+                    piyo
+                </p>
+            </div>
+            <div class="msgboxfoot"></div>
+        </div>
     </div>
+
 @endsection
