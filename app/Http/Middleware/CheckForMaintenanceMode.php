71acf1807a00a965e63d3ee28b20d91e57a718b7
K Miyano <miyano@miyacorata.net>
2020-01-02 18:11:45 +0900

    Update ChackForMaintenanceMode

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Foundation\Http\Exceptions\MaintenanceModeException;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
+use Closure;
+use Symfony\Component\HttpFoundation\IpUtils;
 
 class CheckForMaintenanceMode extends Middleware
 {
@@ -14,4 +17,19 @@ class CheckForMaintenanceMode extends Middleware
     protected $except = [
         //
     ];
+
+    public function handle($request, Closure $next)
+    {
+        if ($this->app->isDownForMaintenance()) {
+            $data = json_decode(file_get_contents($this->app->storagePath().'/framework/down'), true);
+
+            if (isset($data['allowed']) && IpUtils::checkIp($request->ip(), (array) $data['allowed'])) {
+                return $next($request);
+            }
+
+            throw new MaintenanceModeException($data['time'], $data['retry'], $data['message']);
+        }else{
+            return $next($request);
+        }
+    }
 }
