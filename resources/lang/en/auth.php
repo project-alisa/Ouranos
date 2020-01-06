9926113080942f6893cc8cbe7b46b43d5aee39b9
K Miyano <miyano@miyacorata.net>
2020-01-06 21:04:49 +0900

    New translations auth.php (English)

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -12,7 +12,7 @@ return [
     | these language lines according to your application's requirements.
     |
     */
-
+    
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
