7dad2a1af9624ad0c5ec226486f3986388869e24
K Miyano <miyano@miyacorata.net>
2020-01-06 21:04:57 +0900

    New translations mediasearch.php (Chinese Traditional)

---
+++ resources/lang/zh/mediasearch.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    'title' => 'メディア検索',
+
+    'twitter' => 'Twitterを検索',
+    'twitter.normal' => '通常検索',
+    'twitter.normal.desc' => 'Twitter Search',
+    'twitter.realtime' => 'リアルタイム検索',
+    'twitter.realtime.desc' => 'すべてのツイートを時系列順に検索',
+    'twitter.user' => 'ユーザー検索',
+    'twitter.user.desc' => 'プロフィールからユーザーを検索',
+    'twitter.image' => '画像検索',
+    'twitter.image.desc' => '画像を含むツイートを検索',
+    'twitter.video' => '動画検索',
+    'twitter.video.desc' => '動画を含むツイートを検索',
+    'twitter.azure' => '蒼天画廊検索',
+    'twitter.azure.desc' => 'イラストを含むツイートを検索',
+    'twitter.notice.0' => 'Twitter検索は愛称・ニックネームにより表示されるべき結果を表示できない可能性があります。',
+    'twitter.notice.1' => '蒼天画廊(azure-gallery.net)はファンアートや創作に関するツイートの検索に特化したウェブサービスです。(Twitter公式や宮野路快速の提供するサービスではありません)',
+
+    'niconico' => 'niconicoを検索',
+    'niconico.keyword' => '動画をキーワード検索',
+    'niconico.tag' => '動画をタグ検索',
+    'niconico.seiga' => '静画をタグ検索',
+
+    'pixiv' => 'Pixivを検索',
+    'pixiv.tag' => 'タグでイラスト検索',
+    'pixiv.tag.desc' => 'Pixiv総合検索',
+    'pixiv.keyword' => 'キーワードでイラスト検索',
+    'pixiv.keyword.desc' => 'Pixiv総合検索',
+    'pixiv.novel' => 'タグで小説検索',
+    'pixiv.novel.desc' => 'Pixiv小説検索',
+];
