ca7a2843fd7f6296de3790cc7966b49b6b7f7ce5
K Miyano <miyano@miyacorata.net>
2020-01-06 21:04:56 +0900

    New translations messages.php (Chinese Traditional)

---
+++ resources/lang/zh/messages.php
@@ -0,0 +1,42 @@
+<?php
+return [
+    '400' => '要求の形式が正しくありません',
+    '403' => 'このページにアクセスする権限がありません',
+    '404' => 'お探しのページは存在しないか、メメントしてしまいました',
+    '500' => 'サーバ内部でエラーが発生しました',
+    '503' => 'アクセス集中かメンテナンスのため一時的にページを表示できません',
+
+    'common.numbersof' => ':typeの人数',
+
+    'home.description' => 'MillionLivePortalは、アイドルマスターミリオンライブ！の非公式データベース＆ポータルサイトです。',
+    'home.latestnews' => '最新情報',
+    'home.mastodon' => '更新情報は宮野駅(Mastodonインスタンス)から配信しています。',
+    'home.mastodon.error' => 'Mastodonからお知らせを取得できませんでした',
+
+    'idol.index.desc' => 'アイドルを選んでください',
+    'idol.index.incorrect' => '存在しない属性です',
+    'idol.index.typedetail' => ':typeアイドルの概要',
+    'idol.index.typeselect' => '属性で絞り込む',
+
+    'idol.show.desc' => 'アイドルの情報を確認することができます',
+    'idol.show.dbid' => 'データベースID ゲーム内の内部IDと同一です',
+    'idol.show.type' => 'シアターデイズのアイドル属性です',
+    'idol.show.mltd.ja' => '日本語版',
+    'idol.show.mltd.oversea' => '海外版',
+    'idol.show.mltd.ja.cards' => 'ミリシタ カード一覧',
+    'idol.show.mltd.oversea.none' => '海外版にはまだ登場していません',
+
+    'search.index.desc' => '様々な条件からアイドルを検索できます',
+    'search.result.found' => ':count人のアイドルが見つかりました',
+    'search.result.query' => '検索クエリ',
+    'search.result.query.info' => '以下の条件による検索結果です',
+    'search.result.notfound' => '条件を満たすアイドルが見つかりませんでした',
+    'search.redirect' => 'リダイレクト:検索結果が1件でした',
+    'search.error.range.empty' => '年齢の検索範囲指定がありません',
+    'search.error.range.invalid' => '年齢の検索範囲指定が正しくありません',
+    'search.error.birthdate.invalid' => '月か日どちらかは指定してください',
+
+    'share.title' => 'Fediverseで共有',
+    'share.input' => '共有するインスタンスのホストを入力してください',
+    'share.select' => 'または以下から選ぶことができます',
+];
