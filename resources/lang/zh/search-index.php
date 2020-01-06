8c12542bd5d8f60d23d5fe00c9c39767c8fe7f43
K Miyano <miyano@miyacorata.net>
2020-01-06 21:04:53 +0900

    New translations search-index.php (Chinese Traditional)

---
+++ resources/lang/zh/search-index.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'howtouse' => '各種条件単体で検索するか、必要ないくつかの条件を入力しそれらすべてを考慮して検索することができます。',
+    'searchbyallcond' => '複数の条件で検索',
+
+    'name.header' => '名前で検索',
+    'name.notice.0' => '漢字・読みで検索できます。',
+    'name.notice.1' => '検索結果が1件の場合は該当するアイドルのページに自動的に遷移します。',
+
+    'birthplace.header' => '出身地で検索',
+    'birthplace.notice' => 'アイドルの出身地で検索します。',
+
+    'birthdate.header' => '誕生日で検索',
+    'birthdate.notice.0' => '-のままにすることで月のみ、日のみの検索もできます。',
+    'birthdate.notice.1' => '(例：8月生まれすべてを検索、月を指定せず27日生まれを検索)',
+    'birthdate.notice.2' => '月か日どちらかは必ず指定してください。',
+
+    'age.header' => '年齢で検索',
+    'age.notice' => '年齢で検索できます。整数を入力してください。',
+    'age.input' => '年齢 (整数)',
+    'age.range' => '年齢の範囲',
+    'age.range.older' => '以上',
+    'age.range.equal' => 'である',
+    'age.range.younger' => '以下',
+];
