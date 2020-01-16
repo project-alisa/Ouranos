99ebef8a753da6593a355b013650233674c46572
K Miyano <miyano@miyacorata.net>
2020-01-16 14:22:37 +0900

    New translations mediasearch.php (English)

---
+++ resources/lang/en-US/mediasearch.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    'title' => 'Search medias',
+
+    'twitter' => 'Twitter',
+    'twitter.normal' => 'Normal search',
+    'twitter.normal.desc' => 'Twitter Search',
+    'twitter.realtime' => 'Live',
+    'twitter.realtime.desc' => 'Search tweets in chronological order',
+    'twitter.user' => 'User',
+    'twitter.user.desc' => 'Search users by profile',
+    'twitter.image' => 'Image',
+    'twitter.image.desc' => 'Search tweets containing images',
+    'twitter.video' => 'Video',
+    'twitter.video.desc' => 'Search tweets containing videos',
+    'twitter.azure' => 'Azure-Gallery.net',
+    'twitter.azure.desc' => 'Search tweets containing illustrations',
+    'twitter.notice.0' => 'Twitter search may not show results that should be displayed.',
+    'twitter.notice.1' => 'Azure-Gallery.net is a web service that specializes in searching for tweets about fan art and creation. (note : Twitter and MiyanojiRapid are not operators of this site.)',
+
+    'niconico' => 'niconico (nicovideo.jp)',
+    'niconico.keyword' => 'Search videos by keyword',
+    'niconico.tag' => 'Search videos by tag',
+    'niconico.seiga' => 'Search seiga by tag',
+
+    'pixiv' => 'Pixiv',
+    'pixiv.tag' => 'Search by tag',
+    'pixiv.tag.desc' => 'Pixiv comprehensive search',
+    'pixiv.keyword' => 'Search by keyword',
+    'pixiv.keyword.desc' => 'Pixiv comprehensive search',
+    'pixiv.novel' => 'Search novels by tag',
+    'pixiv.novel.desc' => 'Pixiv novel search',
+];
