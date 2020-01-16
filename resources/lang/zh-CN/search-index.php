a09529591c657765b1c39b2c6d40e8fdf2751967
K Miyano <miyano@miyacorata.net>
2020-01-16 14:21:50 +0900

    New translations search-index.php (Chinese Simplified)

---
+++ resources/lang/zh-CN/search-index.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'howtouse' => '可以单条件搜索，也可以多条件搜索',
+    'searchbyallcond' => '多条件搜索',
+
+    'name.header' => '姓名搜索',
+    'name.notice.0' => '可以用汉字和假名搜索',
+    'name.notice.1' => '如果只有一个搜索结果，会自动跳转到该偶像的页面',
+
+    'birthplace.header' => '家乡搜索',
+    'birthplace.notice' => '用偶像的家乡搜索',
+
+    'birthdate.header' => '生日搜索',
+    'birthdate.notice.0' => '不指定数值可以只用「月」和「日」搜索',
+    'birthdate.notice.1' => '（例：搜索所有8月出生的偶像、搜索所有月份27号出生的偶像）',
+    'birthdate.notice.2' => '请至少指定一个数值',
+
+    'age.header' => '年龄搜索',
+    'age.notice' => '用年龄搜索。请输入一个整数',
+    'age.input' => '年龄（整数）',
+    'age.range' => '年龄的范围',
+    'age.range.older' => '以上',
+    'age.range.equal' => '-',
+    'age.range.younger' => '以下',
+];
