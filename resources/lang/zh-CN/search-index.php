764f7c40bdc58eda8044ddd6cf10d5ccd31fc681
K Miyano <miyano@miyacorata.net>
2020-01-16 15:04:38 +0900

    New translations search-index.php (Chinese Simplified)

--- resources/lang/zh-CN/search-index.php
+++ resources/lang/zh-CN/search-index.php
@@ -5,13 +5,13 @@ return [
 
     'name.header' => '姓名搜索',
     'name.notice.0' => '可以用汉字和假名搜索',
-    'name.notice.1' => '如果只有一个搜索结果，会自动跳转到该偶像的页面',
+    'name.notice.1' => '如果只有一个搜索结果，将自动跳转到该偶像的页面',
 
     'birthplace.header' => '家乡搜索',
     'birthplace.notice' => '用偶像的家乡搜索',
 
     'birthdate.header' => '生日搜索',
-    'birthdate.notice.0' => '不指定数值可以只用「月」和「日」搜索',
+    'birthdate.notice.0' => '不指定数值可以只用月份或日期搜索',
     'birthdate.notice.1' => '（例：搜索所有8月出生的偶像、搜索所有月份27号出生的偶像）',
     'birthdate.notice.2' => '请至少指定一个数值',
 
