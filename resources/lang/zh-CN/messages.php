5cde5db0634f2f06c8603c31680c06b019dd3849
K Miyano <miyano@miyacorata.net>
2020-01-16 14:21:53 +0900

    New translations messages.php (Chinese Simplified)

---
+++ resources/lang/zh-CN/messages.php
@@ -0,0 +1,42 @@
+<?php
+return [
+    '400' => '服务器不理解请求的语法',
+    '403' => '您没有权限访问此页面',
+    '404' => '您访问的页面不存在，或已失效',
+    '500' => '服务器内部错误',
+    '503' => '由于访问过多，目前无法加载此页面',
+
+    'common.numbersof' => ':type的人数',
+
+    'home.description' => 'MillionLivePortal是「偶像大师百万现场！」的非官方数据库＆导航站',
+    'home.latestnews' => '最新信息',
+    'home.mastodon' => '最新信息由宮野站（长毛象实例）发布',
+    'home.mastodon.error' => '无法从长毛象获取信息',
+
+    'idol.index.desc' => '请选择偶像',
+    'idol.index.incorrect' => '该属性不存在',
+    'idol.index.typedetail' => '',
+    'idol.index.typeselect' => '属性筛选',
+
+    'idol.show.desc' => '可以查看偶像信息',
+    'idol.show.dbid' => '数据库ID 游戏内ID是一致的',
+    'idol.show.type' => '是剧场时光内的属性',
+    'idol.show.mltd.ja' => '日服',
+    'idol.show.mltd.oversea' => '海外服',
+    'idol.show.mltd.ja.cards' => '',
+    'idol.show.mltd.oversea.none' => '',
+
+    'search.index.desc' => '',
+    'search.result.found' => '',
+    'search.result.query' => '',
+    'search.result.query.info' => '',
+    'search.result.notfound' => '',
+    'search.redirect' => '',
+    'search.error.range.empty' => '',
+    'search.error.range.invalid' => '',
+    'search.error.birthdate.invalid' => '',
+
+    'share.title' => '',
+    'share.input' => '',
+    'share.select' => '',
+];
