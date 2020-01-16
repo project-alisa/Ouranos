ed7e2ea0f32c63d35b22c8f938e16367511d3709
K Miyano <miyano@miyacorata.net>
2020-01-16 13:58:11 +0900

    New translations search-index.php (Chinese Simplified)

--- resources/lang/zh/search-index.php
+++ resources/lang/zh/search-index.php
@@ -1,25 +1,25 @@
 <?php
 return [
-    'howtouse' => '各種条件単体で検索するか、必要ないくつかの条件を入力しそれらすべてを考慮して検索することができます。',
-    'searchbyallcond' => '複数の条件で検索',
+    'howtouse' => '可以单条件搜索，也可以多条件搜索',
+    'searchbyallcond' => '多条件搜索',
 
-    'name.header' => '名前で検索',
-    'name.notice.0' => '漢字・読みで検索できます。',
-    'name.notice.1' => '検索結果が1件の場合は該当するアイドルのページに自動的に遷移します。',
+    'name.header' => '姓名搜索',
+    'name.notice.0' => '可以使用汉字和假名搜索',
+    'name.notice.1' => '如果只有一个搜索结果，会自动跳转到该偶像的页面',
 
-    'birthplace.header' => '出身地で検索',
-    'birthplace.notice' => 'アイドルの出身地で検索します。',
+    'birthplace.header' => '家乡搜索',
+    'birthplace.notice' => '',
 
-    'birthdate.header' => '誕生日で検索',
-    'birthdate.notice.0' => '-のままにすることで月のみ、日のみの検索もできます。',
-    'birthdate.notice.1' => '(例：8月生まれすべてを検索、月を指定せず27日生まれを検索)',
-    'birthdate.notice.2' => '月か日どちらかは必ず指定してください。',
+    'birthdate.header' => '',
+    'birthdate.notice.0' => '',
+    'birthdate.notice.1' => '',
+    'birthdate.notice.2' => '',
 
-    'age.header' => '年齢で検索',
-    'age.notice' => '年齢で検索できます。整数を入力してください。',
-    'age.input' => '年齢 (整数)',
-    'age.range' => '年齢の範囲',
-    'age.range.older' => '以上',
-    'age.range.equal' => 'である',
-    'age.range.younger' => '以下',
+    'age.header' => '',
+    'age.notice' => '',
+    'age.input' => '',
+    'age.range' => '',
+    'age.range.older' => '',
+    'age.range.equal' => '',
+    'age.range.younger' => '',
 ];
