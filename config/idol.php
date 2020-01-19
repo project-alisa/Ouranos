aa653f19a78d8483407e7189ef27859baa60c931
K Miyano <miyano@miyacorata.net>
2020-01-19 12:01:31 +0900

    Add config idol.php

---
+++ config/idol.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'millTokyoNameExceptionList' => [
+        'emilystewart'   => 'emily',
+        'julia'          => 'julia',
+        'mikihoshii'     => 'hoshi-miki',
+        'takaneshijou'   => 'sijo-takane',
+        'elenashimabara' => 'simabara-elena',
+        'annamochizuki'  => 'motizuki-anna',
+        'rocohanda'      => 'roco',
+        'shihokitazawa'  => 'kitagami-shiho',
+        'karenshinomiya' => 'sinomiya-karen',
+        'reikakitakami'  => 'kitagami-reika',
+        'momokosuou'     => 'suo-momoko',
+    ],
+];
