c3afcb8171bf042557d39381873ccb36d6e5d31a
K Miyano <miyano@miyacorata.net>
2020-01-20 00:22:47 +0900

    New translations messages.php (Korean)

---
+++ resources/lang/ko/messages.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    '400' => '요청 형식이 유효하지 않습니다.',
+    '403' => '이 페이지에 액세스 할 수있는 권한이 없습니다.',
+    '404' => '요청한 페이지를이 서버에서 찾을 수 없습니다.',
+    '500' => '인터넷 서버 오류. 불편을 끼쳐 드려 죄송합니다.',
+    '503' => '용량 문제 또는 유지 관리로 인해 서비스를 일시적으로 사용할 수 없습니다.',
+
+    'common.numbersof' => '',
+
+    'home.description' => '',
+    'home.latestnews' => '',
+    'home.mastodon' => '',
+    'home.mastodon.error' => '',
+
+    'idol.index.desc' => '정보를 보려면 idol을 선택하십시오.',
+    'idol.index.incorrect' => '',
+    'idol.index.typedetail' => '',
+    'idol.index.typeselect' => '',
+
+    'idol.show.desc' => '아이돌의 정보를 확인할 수 있습니다.',
+    'idol.show.dbid' => '데이터베이스아이디는 게임내부ID와 동일합니다.',
+    'idol.show.type' => 'MLTD에서의 아이돌의 속성을 나타냅니다.',
+    'idol.show.mltd.ja' => '',
+    'idol.show.mltd.oversea' => '',
+    'idol.show.mltd.ja.cards' => '',
+    'idol.show.mltd.oversea.none' => '',
+    'idol.show.gree.none' => '',
+
+    'search.index.desc' => '',
+    'search.result.found' => '',
+    'search.result.query' => '',
+    'search.result.query.info' => '',
+    'search.result.notfound' => '',
+    'search.redirect' => '',
+    'search.error.range.empty' => '',
+    'search.error.range.invalid' => '',
+    'search.error.birthdate.invalid' => '',
+
+    'share.title' => 'Fediverse 에 공유',
+    'share.input' => '공유 할 인스턴스 호스트를 입력하십시오.',
+    'share.select' => '아래 목록에서 선택할 수도 있습니다.',
+];
