e970bfe11128c47047e6ec0c660dba39e46b076d
K Miyano <miyano@miyacorata.net>
2020-01-22 17:28:36 +0900

    New Crowdin translations (#42)
    * New translations mediasearch.php (Chinese Traditional)
    
    * New translations mediasearch.php (Chinese Traditional)
    
    * New translations auth.php (Korean)

--- resources/lang/zh-TW/mediasearch.php
+++ resources/lang/zh-TW/mediasearch.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'title' => '',
+    'title' => '媒體搜尋',
 
-    'twitter' => '',
-    'twitter.normal' => '',
-    'twitter.normal.desc' => '',
-    'twitter.realtime' => '',
-    'twitter.realtime.desc' => '',
-    'twitter.user' => '',
+    'twitter' => '推特搜尋',
+    'twitter.normal' => '關鍵字搜尋',
+    'twitter.normal.desc' => '推特關鍵字搜尋',
+    'twitter.realtime' => '即時搜尋',
+    'twitter.realtime.desc' => '按時間順序搜尋所有推文',
+    'twitter.user' => '搜尋用戶',
     'twitter.user.desc' => '',
     'twitter.image' => '',
     'twitter.image.desc' => '',
