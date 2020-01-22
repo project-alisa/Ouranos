d56384933943932e90c8a8e38b146e1e679f791f
K Miyano <miyano@miyacorata.net>
2020-01-22 19:10:36 +0900

    Make layouts.idol.blade and Replace

--- resources/views/idol/index.blade.php
+++ resources/views/idol/index.blade.php
@@ -11,32 +11,7 @@
                     $ja_flag = App::isLocale('ja');
                     ?>
                     @foreach ($idols as $idol)
-                        <?php
-                        /** @var App\Idol $idol */
-                        $icon = asset('image/icon/'.$idol->name_r.'/0.png');
-                        if(!$ja_flag){
-                            $name = 'name_'.((App::isLocale('en') || App::isLocale('zh-CN')) ? 'r' : mb_substr($current_lang,0,2));
-                            if(empty($idol->$name)) $name = 'name_r'; //fallback
-                            $separate = $name.'_separate';
-                            $text = e(ucwords(separateString($idol->$name,$idol->$separate)));
-                            $text .= "<span class='ja' style='font-size: 15px;color: dimgray;margin-left: 15px'>".e(ucwords(separateString($idol->name,$idol->name_separate)))."</span>";
-                        }
-                        $dateflag = $ja_flag ? 'ja' : 'slash';
-                        ?>
-                        <a href="{{ url('/idol/'.$idol->name_r) }}" class="idol">
-                            <img src="{{ $icon }}" class="idolicon" alt="icon" style="border-color: {{ getTypeColor($idol->type) }}">
-                            <div class="idolinfo">
-                                <p class="name">{!! $ja_flag ? e(separateString($idol->name,$idol->name_separate)) : $text !!}</p>
-                                <table>
-                                    <tr>
-                                        <th>{{ __fb('Type') }}</th><td style="width: 80px;font-weight: bold;color: {{ getTypeColor($idol->type) }}">{{ $idol->type }}</td>
-                                        <th>{{ __fb('Age') }}</th><td style="width: 70px;">{{ $idol->age ?: 'N/A' }}</td>
-                                        <th>{{ __fb('Birthdate') }}</th><td>{{ $idol->birthdate ? convertDateString($idol->birthdate,$dateflag) : 'N/A' }}</td>
-                                        <th>{{ __fb('Color') }}</th><td style="color: {{ '#'.$idol->color }};width: 100px">{{ !empty($idol->color) ? '■ #'.str_replace('#','',$idol->color) : 'N/A' }}</td>
-                                    </tr>
-                                </table>
-                            </div>
-                        </a>
+                        @include('layouts.idol',['idol' => $idol])
                     @endforeach
                 </div>
                 <div class="msgboxfoot"></div>
