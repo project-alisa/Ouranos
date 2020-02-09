992fc32e258a32895216cdb803cc08efc4c9a351
K Miyano <miyano@miyacorata.net>
2020-02-10 02:29:18 +0900

    New translations mediasearch.php (Korean)

--- resources/lang/ko/mediasearch.php
+++ resources/lang/ko/mediasearch.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    'title' => '',
+    'title' => '미디어 검색',
 
-    'twitter' => '',
-    'twitter.normal' => '',
-    'twitter.normal.desc' => '',
-    'twitter.realtime' => '',
+    'twitter' => '트위터 검색',
+    'twitter.normal' => '통상 검색',
+    'twitter.normal.desc' => '트위터 검색',
+    'twitter.realtime' => '실시간 검색',
     'twitter.realtime.desc' => '',
     'twitter.user' => '',
     'twitter.user.desc' => '',
@@ -16,7 +16,7 @@ return [
     'twitter.azure' => '',
     'twitter.azure.desc' => '',
     'twitter.notice.0' => '',
-    'twitter.notice.1' => '',
+    'twitter.notice.1' => 'azure-gallery.net은 팬아트나 창작에 관련된 트윗을 검색하는 것에 특화된 웹사이트입니다. (공식 사이트는 아닙니다.)',
 
     'niconico' => '',
     'niconico.keyword' => '',
