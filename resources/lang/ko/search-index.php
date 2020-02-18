6b2d054bf873b108c6542da569631b304330abfb
K Miyano <miyano@miyacorata.net>
2020-02-19 00:11:49 +0900

    New translations search-index.php (Korean)

--- resources/lang/ko/search-index.php
+++ resources/lang/ko/search-index.php
@@ -4,7 +4,7 @@ return [
     'searchbyallcond' => '여러 조건으로 검색',
 
     'name.header' => '이름으로 검색',
-    'name.notice.0' => '',
+    'name.notice.0' => '한자, 히라가나 또는 알파벳으로 검색할 수 있습니다.',
     'name.notice.1' => '검색 결과가 1개인 경우는 해당 아이돌의 페이지에 자동으로 이동합니다.',
 
     'birthplace.header' => '출생지로 검색',
