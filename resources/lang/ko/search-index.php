26f7f17854dc8972035924646338d8f2a7c0af4a
K Miyano <miyano@miyacorata.net>
2020-02-19 00:21:53 +0900

    New Crowdin translations (#51)
    * New translations messages.php (Korean)
    
    * New translations search-index.php (Korean)
    
    * New translations ja.json (Korean)
    
    * New translations search-index.php (Korean)

--- resources/lang/ko/search-index.php
+++ resources/lang/ko/search-index.php
@@ -4,7 +4,7 @@ return [
     'searchbyallcond' => '여러 조건으로 검색',
 
     'name.header' => '이름으로 검색',
-    'name.notice.0' => '',
+    'name.notice.0' => '한자, 히라가나 또는 알파벳으로 검색할 수 있습니다.',
     'name.notice.1' => '검색 결과가 1개인 경우는 해당 아이돌의 페이지에 자동으로 이동합니다.',
 
     'birthplace.header' => '출생지로 검색',
@@ -12,7 +12,7 @@ return [
 
     'birthdate.header' => '생일로 검색',
     'birthdate.notice.0' => '',
-    'birthdate.notice.1' => '',
+    'birthdate.notice.1' => '(예시: 모든 8월생을 검색, 월을 지정하지 않고 27일생을 검색)',
     'birthdate.notice.2' => '월 또는 일 중 하나는 반드시 지정해 주십시오.',
 
     'age.header' => '나이로 검색',
@@ -20,6 +20,6 @@ return [
     'age.input' => '나이 (정수)',
     'age.range' => '나이의 범위',
     'age.range.older' => '이상',
-    'age.range.equal' => '',
+    'age.range.equal' => '일치',
     'age.range.younger' => '이하',
 ];
