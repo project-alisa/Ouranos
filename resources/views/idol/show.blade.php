855fddc573d08d2aa5fd7131d0bb9bb8ee255bc2
K Miyano <miyano@miyacorata.net>
2020-04-30 01:32:29 +0900

    Fix mill.tokyo link

--- resources/views/idol/show.blade.php
+++ resources/views/idol/show.blade.php
@@ -211,7 +211,7 @@
                         </p>
                     @else
                         <div class="buttonbox">
-                            <a href="https://mill.tokyo/?name={{ genMillTokyoLinkText($idol->name_r,$idol->name_r_separate) }}/"
+                            <a href="https://mill.tokyo/?idol={{ genMillTokyoLinkText($idol->name_r,$idol->name_r_separate) }}"
                                class="button jwil" target="_blank">MillionLive Cards<span class="subline">mill.tokyo</span></a>
                         </div>
                     @endif
