f63bc95ff4b39cd45deb0e759aa707f72145c88d
K Miyano <miyano@miyacorata.net>
2020-05-11 09:42:24 +0900

    Add isCJ()

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -21,3 +21,18 @@ if(!function_exists('changeGetParameter')){
         return $url;
     }
 }
+
+if(!function_exists('isCJ')){
+    /**
+     * @return bool
+     */
+    function isCJ(): bool
+    {
+        $locale = App::getLocale();
+        if($locale === 'ja' || substr($locale,0,2) === 'zh'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
