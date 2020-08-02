c1ba7655372413dc1363c0e8088a1ca63bf9f359
K Miyano <miyano@miyacorata.net>
2020-08-02 19:37:36 +0900

    Remove '/'

--- config/ouranos.php
+++ config/ouranos.php
@@ -16,7 +16,7 @@ return [
 
     'acceptableTypes' => array('Princess','Fairy','Angel','Ex'),
     'mastodonFeedUrl' => 'https://mstdn.miyacorata.net/@ML_Portal.rss',
-    'matsurihimeEndpointUrl' => 'https://api.matsurihi.me/mltd/v1/',
+    'matsurihimeEndpointUrl' => 'https://api.matsurihi.me/mltd/v1',
 
     'repositoryUrl' => 'https://github.com/project-alisa/Ouranos',
 
